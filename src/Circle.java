a01517ff9c0107a779fda005e0cfb13b7cd1c8f6
eln008 <140242882+eln008@users.noreply.github.com>
2023-08-09 11:30:53 +0600

    д

--- src/Circle.java
+++ src/Circle.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Circle {
+public class Circle {
+    public static final double PI = 3.141592653589793;
+
+    public static double area(double radius) {
+        return PI * (radius * radius);
+    }
+
+    public static double circumference(double radius) {
+        return PI * 2 * radius;
+    }
 }
